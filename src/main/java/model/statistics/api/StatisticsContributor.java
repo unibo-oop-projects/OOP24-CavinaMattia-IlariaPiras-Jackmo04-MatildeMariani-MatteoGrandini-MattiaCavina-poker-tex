5146f24d76bcdbfc0f2c43d62141bd53e9474d53
Matteo Grandini <matteo.grandini@studio.unibo.it>
2025-01-24 16:31:24 +0100

    Added Interfaces for managing statistics
    - Added a StatisticsManager interface for putting together all statistics changes coming from StatisticContributor(s)
    - Added javadoc to the StatisticsContributor interface

--- src/main/java/model/statistics/api/StatisticsContributor.java
+++ src/main/java/model/statistics/api/StatisticsContributor.java
@@ -1,6 +1,15 @@
 package model.statistics.api;
 
+/**
+ * Interface for contributing to the game's statistics.
+ * The classes that implement this interface will be able to update the statistics
+ * by implementing the updateStatistics method, which will be called by a {@link StatisticsManager}.
+ */
 public interface StatisticsContributor {
 
+    /**
+     * Method to update the game's statistics.
+     * @param stats The statistics object to update.
+     */
     void updateStatistics(Statistics stats);
 }
---
+++ src/main/java/model/statistics/api/StatisticsManager.java
@@ -0,0 +1,9 @@
+package model.statistics.api;
+
+public interface StatisticsManager {
+
+    void addContributor(StatisticsContributor contributor);
+
+    void updateTotalStatistics();
+
+}
