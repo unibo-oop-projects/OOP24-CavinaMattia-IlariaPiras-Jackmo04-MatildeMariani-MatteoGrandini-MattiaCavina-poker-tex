fb572dda7e26e8a793ea088c73a695ae02cc8a60
Ilaria Piras <ilaria.piras2@studio.unibo.it>
2025-01-26 16:53:31 +0100

    Initial definition of GameFactory

---
+++ src/main/java/model/game/api/GameFactory.java
@@ -0,0 +1,32 @@
+package model.game.api;
+
+import model.player.ai.api.AIPlayer;
+
+/**
+ * Factory to create a {@link Game}. 
+ * It can be of three types depending on the difficulty level of the {@link AIPlayer}s. 
+ */
+public interface GameFactory {
+
+    /**
+     * Returns a new Game with easy difficulty level.
+     * @param initialChips initial amount of chips of players.
+     * @return a new game.
+     */
+    Game easyGame(int initialChips);
+
+    /**
+     * Returns a new Game with medium difficulty level.
+     * @param initialChips initial amount of chips of players.
+     * @return a new game.
+     */
+    Game mediumGame(int initialChips);
+
+    /**
+     * Returns a new Game with difficult difficulty level.
+     * @param initialChips initial amount of chips of players.
+     * @return a new game.
+     */
+    Game difficultGame(int initialChips);
+
+}
