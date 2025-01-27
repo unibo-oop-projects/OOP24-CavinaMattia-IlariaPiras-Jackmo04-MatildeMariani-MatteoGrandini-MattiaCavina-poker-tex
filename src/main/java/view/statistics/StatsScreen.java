b2f09e495234174cb7804ca9ff5992ad506dc4bf
Matteo Grandini <matteo.grandini@studio.unibo.it>
2025-01-27 17:32:15 +0100

    Added possible method for updating statsView

--- src/main/java/view/statistics/StatsScreen.java
+++ src/main/java/view/statistics/StatsScreen.java
@@ -4,6 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.util.Map;
 
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
@@ -13,6 +14,8 @@ import javax.swing.JPanel;
 
 public class StatsScreen extends JPanel implements StatsView {
 
+    private final JPanel statsPanel;
+
     public StatsScreen(CardLayout cardLayout, JPanel mainPanel) {
         this.setLayout(new BorderLayout());
         JLabel title = new JLabel("Statistics", JLabel.CENTER);
@@ -21,19 +24,19 @@ public class StatsScreen extends JPanel implements StatsView {
         this.add(title, BorderLayout.NORTH);
 
 
-        JPanel statsPanel = new JPanel();
+        this.statsPanel = new JPanel();
         statsPanel.setLayout(new BoxLayout(statsPanel, BoxLayout.Y_AXIS));
         statsPanel.setBorder(BorderFactory.createEmptyBorder(10, 100, 10, 100));
 
         // TODO get stats from controller
-        JPanel gamesPlayedPanel = new StatPanel("Games Played: ", 0);
-        JPanel gamesWonPanel = new StatPanel("Games Won: ", 0);
-        JPanel handsPlayedPanel = new StatPanel("Hands Played: ", 0);
-        JPanel handsWonPanel = new StatPanel("Hands Won: ", 0);
-        statsPanel.add(gamesPlayedPanel);
-        statsPanel.add(gamesWonPanel);
-        statsPanel.add(handsPlayedPanel);
-        statsPanel.add(handsWonPanel);
+        // JPanel gamesPlayedPanel = new StatPanel("Games Played: ", 0);
+        // JPanel gamesWonPanel = new StatPanel("Games Won: ", 0);
+        // JPanel handsPlayedPanel = new StatPanel("Hands Played: ", 0);
+        // JPanel handsWonPanel = new StatPanel("Hands Won: ", 0);
+        // statsPanel.add(gamesPlayedPanel);
+        // statsPanel.add(gamesWonPanel);
+        // statsPanel.add(handsPlayedPanel);
+        // statsPanel.add(handsWonPanel);
         
         this.add(statsPanel, BorderLayout.CENTER);
 
@@ -42,11 +45,19 @@ public class StatsScreen extends JPanel implements StatsView {
         this.add(backButton, BorderLayout.SOUTH);
     }
 
+    @Override
+    public void updateStats(Map<String, String> statsMap) {
+        statsMap.entrySet().forEach(e -> {
+            this.statsPanel.removeAll();
+            this.statsPanel.add(new StatPanel(e.getKey(), e.getValue()));
+        });
+    }
+
     // Inner class to create a panel for each statistic (not definitive)
     private class StatPanel extends JPanel {
-        public StatPanel(String name, int value) {
+        public StatPanel(String name, String value) {
             JLabel nameLabel = new JLabel(name);
-            JLabel valueLabel = new JLabel(String.valueOf(value));
+            JLabel valueLabel = new JLabel(value);
             nameLabel.setFont(new Font("Arial", Font.PLAIN, 20));
             valueLabel.setFont(new Font("Arial", Font.PLAIN, 20));
             this.add(nameLabel);
--- src/main/java/view/statistics/StatsView.java
+++ src/main/java/view/statistics/StatsView.java
@@ -1,5 +1,8 @@
 package view.statistics;
 
+import java.util.Map;
+
 public interface StatsView {
 
+    void updateStats(Map<String, String> statsMap);
 }
