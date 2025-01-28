69f4a8261b2f5e1a39331e93510896b7d9d057ee
Ilaria Piras <ilaria.piras2@studio.unibo.it>
2025-01-28 13:36:58 +0100

    Changed Game.java - removed method to sort the players by their roles - fixed documentation

--- src/main/java/model/game/api/Game.java
+++ src/main/java/model/game/api/Game.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import model.dealer.api.Dealer;
 import model.player.api.Player;
-import model.player.api.Role;
 
 /**
  * Interface that models a generic a Game.
@@ -25,11 +24,10 @@ public interface Game {
     public boolean isWon();
 
     /**
-     * A new hand begins until isOver returns true, checking which {@link Player}s are still in the
-     * game, sorting them by their {@link Role}s and asking the {@link Dealer} to deal cards to
-     * each of them. It then goes through each {@link Phase} of the hand until it reaches the last 
-     * one or until all but one player folds, asking the dealer for the community cards and the players for
-     * their action. At the end of each hand it declares the winner.   
+     * A new hand begins until isOver returns true, updating the list of {@link Player}s (checking which ones
+     * are still in the game) and their {@link Role}s, and asking the {@link Dealer} to deal cards to each of them. 
+     * It then goes through each {@link Phase} of the hand until it ends. 
+     * The winner of the hand is then declared. 
     */
     public void start();
     
@@ -44,14 +42,5 @@ public interface Game {
      * @return the game State.
      */
     public State getGameState();
-
-    /**
-     * Sort the given list of {@link Player}s, placing the player with the given {@link Role} first, 
-     * then the players that were after him in the original list, and lastly those who were before him,
-     * in the original order.
-     * @param players the list of players to order.
-     * @param firstPlayerRole the role of the player that must be set first of the list.
-     */
-    void sortFromRole(final List<Player> players, final Role firstPlayerRole);
 
 }
