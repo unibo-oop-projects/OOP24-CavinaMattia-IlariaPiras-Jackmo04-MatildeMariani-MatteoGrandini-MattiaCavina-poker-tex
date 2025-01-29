690d8574d78028afa7c16f3594487443196acfa9
Ilaria Piras <ilaria.piras2@studio.unibo.it>
2025-01-29 11:25:03 +0100

    Removed player sorting test in TestGame

--- src/test/java/main/model/game/TestGame.java
+++ src/test/java/main/model/game/TestGame.java
@@ -4,31 +4,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import model.game.GameFactoryImpl;
 import model.game.api.GameFactory;
-import model.player.ai.AIPlayerFactoryImpl;
-import model.player.ai.api.AIPlayerFactory;
-import model.player.api.Player;
-import model.player.api.Role;
-
 public class TestGame {
 
     private static final int INITIAL_CHIPS = 500;
     private static final int INITIAL_NUM_PLAYERS = 3;
 
     private static GameFactory gameFactory;
-    private static AIPlayerFactory playerFactory;
 
     @BeforeAll
     public static void setUp() {
         gameFactory = new GameFactoryImpl();
-        playerFactory = new AIPlayerFactoryImpl();
     }
 
     @Test
@@ -51,20 +41,5 @@ public class TestGame {
 
     }
 
-    @Test
-    public void testSortFromRole() {
-        var game = gameFactory.easyGame(500);
-        Player player1 = playerFactory.easy(INITIAL_CHIPS, Role.DEALER);
-        Player player2 = playerFactory.easy(INITIAL_CHIPS, Role.SMALL_BLIND);
-        Player player3 = playerFactory.easy(INITIAL_CHIPS, Role.BIG_BLIND);
-        Player player4 = playerFactory.easy(INITIAL_CHIPS, Role.REGULAR);
-
-        var players = new ArrayList<>(List.of(player1, player2, player3, player4));
-        game.sortFromRole(players, Role.SMALL_BLIND);
-        assertEquals(List.of(player2, player3, player4, player1), players);
-
-        game.sortFromRole(players, Role.BIG_BLIND);
-        assertEquals(List.of(player3, player4, player1, player2), players);
-    }
 
 }
