255f79964ccf5e9d5618f0d17daf1584b0cc7a3a
Matilde Mariani <matilde.mariani3@studio.unibo.it>
2025-01-30 11:04:54 +0100

    changed aspects related to player's role

--- src/main/java/model/player/user/UserPlayer.java
+++ src/main/java/model/player/user/UserPlayer.java
@@ -10,8 +10,6 @@ import model.game.api.Phase;
 import model.game.api.State;
 import model.player.AbstractPlayer;
 import model.player.api.Action;
-import model.player.api.Role;
-import model.temp.Blind;
 
 /**
  * Class representing a human player in the game.
@@ -26,8 +24,8 @@ public class UserPlayer extends AbstractPlayer {
      * @param initialChips the initial amount of chips that the player has.
      * @param initialRole the initial role of the player.
      */
-    public UserPlayer(final int initialChips, final Role initialRole) {
-        super(initialChips, initialRole);
+    public UserPlayer(final int initialChips) {
+        super(initialChips);
         this.controller = new UserPlayerController(this);
         this.setTotalPhaseBet(INITIAL_TOTAL_PHASE_BET);
     }
@@ -42,14 +40,8 @@ public class UserPlayer extends AbstractPlayer {
             throw new IllegalStateException("Player must have 2 cards to play");
         }
         this.updateCombination(currentState);
-        if(currentState.getHandPhase() == Phase.PREFLOP && this.getTotalPhaseBet() == 0 && 
-        (this.getRole() == Role.BIG_BLIND || this.getRole() == Role.SMALL_BLIND)) {
-            if(this.getRole() == Role.SMALL_BLIND) {
-                this.setTotalPhaseBet((int) (currentState.getCurrentBet() * Blind.SMALL.getMultiplier()));
-            } else if(this.getRole() == Role.BIG_BLIND) {
-                this.setTotalPhaseBet((int) (currentState.getCurrentBet() * Blind.BIG.getMultiplier()));
-            } //--> da modificare 
-            //this.setTotalPhaseBet(currentState.getCurrentBet() * this.getRole().getMultiplier());
+        if(currentState.getHandPhase() == Phase.PREFLOP && this.getTotalPhaseBet() == 0 && this.getRole().isPresent()) {
+            this.setTotalPhaseBet((int) (currentState.getCurrentBet() * this.getRole().get().getMultiplier()));
             this.setChips(this.getChips() - this.getTotalPhaseBet());
             return Action.CALL;
         } else { 
