1a4479f2b712fa1ad9c4fa714ac9ec61e03a4d65
Ilaria Piras <ilaria.piras2@studio.unibo.it>
2025-01-31 18:15:03 +0100

    Changed AIPlayerPanel.java - added GridBagLayout - changed colors - added constants for width and height

--- src/main/java/view/panels/AIPlayerPanel.java
+++ src/main/java/view/panels/AIPlayerPanel.java
@@ -2,11 +2,18 @@ package view.panels;
 
 import java.awt.Color;
 import java.awt.GridLayout;
+import java.awt.GridBagLayout;
 
+import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JPanel;
 
 public class AIPlayerPanel extends JPanel {
+
+    private static final int PLAYER_IMAGE_WIDTH = 50;
+    private static final int PLAYER_IMAGE_HEIGHT = 50;
+    private static final int CARD_HEIGHT = 70;
+    private static final int CARD_WIDTH = 50;
 
     /**
      * Constructor for the AIPlayerPanel class.
@@ -18,18 +25,17 @@ public class AIPlayerPanel extends JPanel {
         JPanel cardsPanel = new JPanel(new GridLayout(1, 2));
         MyLabel dxCard = new MyLabel("");
         MyLabel sxCard = new MyLabel("");
-        dxCard.setSize(50, 70);
-        sxCard.setSize(50, 70);
+        dxCard.setSize(CARD_WIDTH, CARD_HEIGHT);
+        sxCard.setSize(CARD_WIDTH, CARD_HEIGHT);
         dxCard.setImage("retroCarta.png");
         sxCard.setImage("retroCarta.png");
         cardsPanel.add(sxCard);
         cardsPanel.add(dxCard);
-        cardsPanel.setBackground(Color.CYAN);
-        cardsPanel.setOpaque(true);
+        cardsPanel.setBackground(Color.LIGHT_GRAY);
 
         /*Creation player image label*/
         MyLabel playerImage = new MyLabel("");
-        playerImage.setSize(50, 50);
+        playerImage.setSize(PLAYER_IMAGE_WIDTH, PLAYER_IMAGE_HEIGHT);
         playerImage.setImage("hat.png");
 
         /*Creation images panel, it contains the player image and his set of cards*/
@@ -37,28 +43,31 @@ public class AIPlayerPanel extends JPanel {
         imagesPanel.setLayout(new BoxLayout(imagesPanel, BoxLayout.X_AXIS));
         imagesPanel.add(playerImage);
         imagesPanel.add(cardsPanel);
-        imagesPanel.setBackground(Color.CYAN);
-        imagesPanel.setOpaque(true);
+        imagesPanel.setBackground(Color.LIGHT_GRAY);
 
         /*Creation data Panel, it contains the basic data of a player*/
         JPanel dataPanel = new JPanel();
         dataPanel.setLayout(new BoxLayout(dataPanel, BoxLayout.X_AXIS));
-        
+            
         MyLabel playerAction = new MyLabel("ACTION ");
         MyLabel playerChips = new MyLabel(" Chips: 2000 ");
         MyLabel playerRole = new MyLabel(" SB");
         dataPanel.add(playerAction);
         dataPanel.add(playerChips);
         dataPanel.add(playerRole);
-        dataPanel.setBackground(Color.CYAN);
-        dataPanel.setOpaque(true);
+        dataPanel.setBackground(Color.LIGHT_GRAY);
 
         /*Adding panels to the main one*/
-        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        this.add(imagesPanel);
-        this.add(dataPanel);
-        this.setBackground(Color.CYAN);
-        this.setOpaque(true);
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+        mainPanel.add(imagesPanel);
+        mainPanel.add(dataPanel);
+        mainPanel.setBackground(Color.LIGHT_GRAY);
+        mainPanel.setOpaque(true);
+        mainPanel.setBorder(BorderFactory.createLineBorder(Color.GRAY, 5, true));
+
+        this.setLayout(new GridBagLayout());
+        this.add(mainPanel);
 
     }
 
