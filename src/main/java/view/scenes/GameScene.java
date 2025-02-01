c2f7eab34183ee59ee7040b1e4647b2a34719e6f
Ilaria Piras <ilaria.piras2@studio.unibo.it>
2025-02-01 16:31:24 +0100

    Added panels and background color to GameScene

--- src/main/java/view/scenes/GameScene.java
+++ src/main/java/view/scenes/GameScene.java
@@ -1,11 +1,14 @@
 package view.scenes;
 
+
 import java.awt.BorderLayout;
-import java.awt.FlowLayout;
+import java.awt.Color;
 
 import javax.swing.JPanel;
 
 import controller.game.GameController;
+import view.panels.AIPlayerPanel;
+import view.panels.TablePanel;
 import view.scenes.api.Scene;
 
 public class GameScene extends JPanel implements Scene {
@@ -15,36 +18,46 @@ public class GameScene extends JPanel implements Scene {
     private final GameController controller;
 
     public GameScene(final GameController controller) {
+
         this.controller = controller;
         this.setLayout(new BorderLayout());
+        this.setBackground(Color.DARK_GRAY);
+        this.setOpaque(true);
 
+        AIPlayerPanel west = new AIPlayerPanel();
+        AIPlayerPanel north = new AIPlayerPanel();
+        AIPlayerPanel east = new AIPlayerPanel();
+        /*To change with a UserPlayerPanel */
+        AIPlayerPanel south = new AIPlayerPanel();  
+        TablePanel table = new TablePanel();
+
+        west.setBackground(Color.DARK_GRAY);
+        north.setBackground(Color.DARK_GRAY);
+        east.setBackground(Color.DARK_GRAY);
+        south.setBackground(Color.DARK_GRAY);
+
+        this.add(north, BorderLayout.NORTH);
+        this.add(west, BorderLayout.WEST);
+        this.add(east, BorderLayout.EAST);
+        this.add(south, BorderLayout.SOUTH);
+        this.add(table, BorderLayout.CENTER);
 
     }
 
-
-    private class AIPlayerPanel extends JPanel{
-
-        AIPlayerPanel() {
-
-        }
-    }
-
-    private class TablePanel extends JPanel{
-
-        TablePanel() {
-
-        }
-    }
-
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public JPanel getPanel() {
         return this;
     }
 
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String getSceneName() {
         return SCENE_NAME;
     }
+
 }
