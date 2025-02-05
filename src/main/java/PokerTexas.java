8540a890eb5ba3d66e6e77bcad33d1d54e4cf194
Matteo Grandini <matteo.grandini@studio.unibo.it>
2025-02-05 17:22:20 +0100

    Reformulated todo comment

--- src/main/java/PokerTexas.java
+++ src/main/java/PokerTexas.java
@@ -3,7 +3,7 @@ import view.ViewImpl;
 /**
  * Main class of the application.
  */
-public final class PokerTexas { // TODO: create a package for this class
+public final class PokerTexas { // TODO: create a package for the project
 
     private PokerTexas() {
     }
