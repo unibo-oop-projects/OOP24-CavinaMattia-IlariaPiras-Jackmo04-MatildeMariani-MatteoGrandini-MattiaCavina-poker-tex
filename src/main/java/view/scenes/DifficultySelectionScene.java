e78a1c049cc69481210f7c38a921b74c5d8b4fa9
Matilde Mariani <matilde.mariani3@studio.unibo.it>
2025-02-06 20:49:22 +0100

    Changes: - renamed and fixed MyButton class - changed getPanel method - created initialize method

--- src/main/java/view/scenes/DifficultySelectionScene.java
+++ src/main/java/view/scenes/DifficultySelectionScene.java
@@ -19,6 +19,9 @@ import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import controller.difficulty.DifficultySelectionController;
 import controller.game.api.Difficulty;
 import view.scenes.api.Scene;
@@ -30,12 +33,12 @@ import view.scenes.api.Scene;
  * and a text field for entering the initial number of chips.
  * The user can proceed to the game scene by pressing the "Play" button.
  */
-public class DifficultySelectionScene extends JPanel implements Scene {
+public class DifficultySelectionScene implements Scene {
 
     private static final int R_BUTTONS_PANEL = 236;
     private static final int G_BUTTONS_PANEL = 205;
     private static final int B_BUTTONS_PANEL = 153;
-    private static final int R_BORDER = 0;  
+    private static final int R_BORDER = 0;
     private static final int G_BORDER = 0;
     private static final int B_BORDER = 0;
     private static final int A_BORDER = 50;
@@ -52,58 +55,68 @@ public class DifficultySelectionScene extends JPanel implements Scene {
     private static final int B_INPUT_PANEL = 208;
     private static final int BUTTON_WIDTH = 150;
     private static final int BUTTON_HEIGHT = 50;
+    private static final String FONT = "Roboto";
     private static final String SCENE_NAME = "difficulty selection";
 
     private final DifficultySelectionController controller;
+    private final JPanel diffSelPanel;
+    private static final Logger LOGGER = LoggerFactory.getLogger(DifficultySelectionScene.class);
 
     /**
      * Constructs a new DifficultySelectionScene.
      * @param controller the controller that handles the difficulty selection logic.
      */
     public DifficultySelectionScene(final DifficultySelectionController controller) { 
+        this.diffSelPanel = new JPanel(new BorderLayout());
         this.controller = controller;
-        this.setLayout(new BorderLayout());
+        initialize();
+    }
 
-        JPanel mainPanel = new JPanel();
+    private void initialize() {
+        final JPanel mainPanel = new JPanel();
         mainPanel.setLayout(new GridBagLayout());
         mainPanel.setBackground(new Color(R_BACKGROUND, G_BACKGROUND, B_BACKGROUND));
 
-        JPanel titlePanel = new JPanel();
+        final JPanel titlePanel = new JPanel();
         titlePanel.setLayout(new FlowLayout());
         titlePanel.setBackground(new Color(R_BACKGROUND, G_BACKGROUND, B_BACKGROUND));
 
-        JLabel title = new JLabel("DIFFICULTY");
-        title.setFont(new Font("Roboto", Font.BOLD, FONT_SIZE_TITLE));
+        final JLabel title = new JLabel("DIFFICULTY");
+        title.setFont(new Font(FONT, Font.BOLD, FONT_SIZE_TITLE));
         titlePanel.add(title);
 
-        JPanel centerPanel = new JPanel();
+        final JPanel centerPanel = new JPanel();
         centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.Y_AXIS));
 
-        JPanel inputPanel = new JPanel();
-        inputPanel.setLayout(new GridLayout(3,1));
+        final JPanel inputPanel = new JPanel();
+        inputPanel.setLayout(new GridLayout(3, 1));
 
-        JPanel buttonsPanel = new JPanel();
-        buttonsPanel.setLayout(new GridLayout(3,1));
+        final JPanel buttonsPanel = new JPanel();
+        buttonsPanel.setLayout(new GridLayout(3, 1));
         buttonsPanel.setBackground(new Color(R_BACKGROUND, G_BACKGROUND, B_BACKGROUND));
 
-        JRadioButton easy = new JRadioButton("EASY");
-        easy.setFont(new Font("Roboto", Font.BOLD, FONT_SIZE_BUTTON));
+        final JRadioButton easy = new JRadioButton("EASY");
+        easy.setFont(new Font(FONT, Font.BOLD, FONT_SIZE_BUTTON));
         easy.setHorizontalAlignment(SwingConstants.CENTER);
 
-        JRadioButton medium = new JRadioButton("MEDIUM");
-        medium.setFont(new Font("Roboto", Font.BOLD, FONT_SIZE_BUTTON));
+        final JRadioButton medium = new JRadioButton("MEDIUM");
+        medium.setFont(new Font(FONT, Font.BOLD, FONT_SIZE_BUTTON));
         medium.setHorizontalAlignment(SwingConstants.CENTER);
 
-        JRadioButton hard = new JRadioButton("HARD");
-        hard.setFont(new Font("Roboto", Font.BOLD, FONT_SIZE_BUTTON));
+        final JRadioButton hard = new JRadioButton("HARD");
+        hard.setFont(new Font(FONT, Font.BOLD, FONT_SIZE_BUTTON));
         hard.setHorizontalAlignment(SwingConstants.CENTER);
 
-        ActionListener difficultyListener = e -> {
-            JRadioButton source = (JRadioButton) e.getSource();
+        final ActionListener difficultyListener = e -> {
+            final JRadioButton source = (JRadioButton) e.getSource();
             switch (source.getText()) {
                 case "EASY" -> this.controller.setDifficulty(Difficulty.EASY);
                 case "MEDIUM" -> this.controller.setDifficulty(Difficulty.MEDIUM);
                 case "HARD" -> this.controller.setDifficulty(Difficulty.HARD);
+                default -> {
+                    LOGGER.info("Invalid difficulty, default: EASY");
+                    this.controller.setDifficulty(Difficulty.EASY);
+                }
             }
         };
 
@@ -111,7 +124,7 @@ public class DifficultySelectionScene extends JPanel implements Scene {
         medium.addActionListener(difficultyListener);
         hard.addActionListener(difficultyListener);
 
-        ButtonGroup group = new ButtonGroup();
+        final ButtonGroup group = new ButtonGroup();
         group.add(easy);
         group.add(medium);
         group.add(hard);
@@ -120,17 +133,17 @@ public class DifficultySelectionScene extends JPanel implements Scene {
         buttonsPanel.add(medium);
         buttonsPanel.add(hard);
 
-        JPanel initialChipsPanel = new JPanel();
-        initialChipsPanel.setLayout(new GridLayout(2,1));
+        final JPanel initialChipsPanel = new JPanel();
+        initialChipsPanel.setLayout(new GridLayout(2, 1));
 
-        JLabel initialChipsLabel = new JLabel("How many chips do you want to start with?");
-        initialChipsLabel.setFont(new Font("Roboto", Font.BOLD, FONT_SIZE_LABEL));
+        final JLabel initialChipsLabel = new JLabel("How many chips do you want to start with?");
+        initialChipsLabel.setFont(new Font(FONT, Font.BOLD, FONT_SIZE_LABEL));
         initialChipsLabel.setBackground(new Color(R_BACKGROUND, G_BACKGROUND, B_BACKGROUND));
         initialChipsLabel.setOpaque(true);
         initialChipsLabel.setHorizontalAlignment(SwingConstants.CENTER);
 
-        JTextField input = new JTextField("", TEXT_FIELD_SIZE);
-        input.setFont(new Font("Roboto", Font.BOLD, FONT_SIZE_LABEL));
+        final JTextField input = new JTextField("", TEXT_FIELD_SIZE);
+        input.setFont(new Font(FONT, Font.BOLD, FONT_SIZE_LABEL));
         input.setBackground(new Color(R_INPUT_PANEL, G_INPUT_PANEL, B_INPUT_PANEL));
         input.addActionListener(e -> {
             this.controller.setInitialChips(Integer.parseInt(input.getText()));
@@ -141,17 +154,17 @@ public class DifficultySelectionScene extends JPanel implements Scene {
         initialChipsPanel.add(initialChipsLabel);
         initialChipsPanel.add(input);
 
-        JPanel playPanel = new JPanel();
+        final JPanel playPanel = new JPanel();
         playPanel.setLayout(new FlowLayout());
         playPanel.setBackground(new Color(R_BACKGROUND, G_BACKGROUND, B_BACKGROUND));
 
-        MyButton play = new MyButton("PLAY");
+        final DiffSelButton play = new DiffSelButton("PLAY");
 
-        play.addActionListener(e -> {
+        play.getButton().addActionListener(e -> {
             this.controller.goToGameScene();
         });
-        
-        playPanel.add(play);
+
+        playPanel.add(play.getButton());
 
         inputPanel.add(buttonsPanel);
         inputPanel.add(initialChipsPanel);
@@ -160,11 +173,11 @@ public class DifficultySelectionScene extends JPanel implements Scene {
         centerPanel.add(titlePanel);
         centerPanel.add(inputPanel);
         mainPanel.add(centerPanel);
-        this.add(mainPanel, BorderLayout.CENTER);
+        this.diffSelPanel.add(mainPanel, BorderLayout.CENTER);
 
-        MyButton backButton = new MyButton("Back to Menu");
-        backButton.addActionListener(e -> this.controller.goToMainMenuScene());
-        this.add(backButton, BorderLayout.SOUTH);
+        final DiffSelButton backButton = new DiffSelButton("Back to Menu");
+        backButton.getButton().addActionListener(e -> this.controller.goToMainMenuScene());
+        this.diffSelPanel.add(backButton.getButton(), BorderLayout.SOUTH);
     }
 
     /**
@@ -172,7 +185,9 @@ public class DifficultySelectionScene extends JPanel implements Scene {
      */
     @Override
     public JPanel getPanel() {
-        return this;
+        final var wrapper = new JPanel(new BorderLayout());
+        wrapper.add(this.diffSelPanel, BorderLayout.CENTER);
+        return wrapper;
     }
 
     /**
@@ -187,18 +202,28 @@ public class DifficultySelectionScene extends JPanel implements Scene {
      * Custom button class for the DifficultySelectionScene.
      * This class extends JButton and provides a style for buttons in this scene.
      */
-    private class MyButton extends JButton {
+    private static class DiffSelButton {
 
-        public MyButton(String text) {
-            super(text);
-            this.setBackground(new Color(R_BUTTONS_PANEL, G_BUTTONS_PANEL, B_BUTTONS_PANEL));
-            this.setForeground(Color.BLACK);
-            this.setFont(new Font("Roboto", Font.BOLD, FONT_SIZE_BUTTON));
-            this.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createRaisedBevelBorder(), 
+        private final JButton button;
+
+        DiffSelButton(final String text) {
+            button = new JButton(text);
+            initializeButton();
+        }
+
+        private void initializeButton() {
+            this.button.setBackground(new Color(R_BUTTONS_PANEL, G_BUTTONS_PANEL, B_BUTTONS_PANEL));
+            this.button.setForeground(Color.BLACK);
+            this.button.setFont(new Font(FONT, Font.BOLD, FONT_SIZE_BUTTON));
+            this.button.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createRaisedBevelBorder(), 
                 BorderFactory.createLineBorder(new Color(R_BORDER, G_BORDER, B_BORDER, A_BORDER), THICKNESS, true)));
-            this.setOpaque(true);
-            this.setContentAreaFilled(true);
-            this.setPreferredSize(new Dimension(BUTTON_WIDTH, BUTTON_HEIGHT));
+            this.button.setOpaque(true);
+            this.button.setContentAreaFilled(true);
+            this.button.setPreferredSize(new Dimension(BUTTON_WIDTH, BUTTON_HEIGHT));
+        }
+
+        public JButton getButton() {
+            return this.button;
         }
     }
 }
