d1a3cb3160c121d8bcde8a93a55e18e01fd37786
Mattia Cavina <mattia.cavina2@studio.unibo.it>
2025-02-08 09:56:10 +0100

    Fix CardGetterImage Javadoc

--- src/main/java/controller/card/CardGetterImage.java
+++ src/main/java/controller/card/CardGetterImage.java
@@ -8,29 +8,25 @@ import javax.swing.ImageIcon;
 import model.deck.api.Card;
 
 /**
- * Inteface to transfom {@link Set} of {@link model.deck.api.Card} in its
+ * Inteface to transfom {@link Set} of {@link Card} in its
  * respective Immage to be showed.
  */
 public interface CardGetterImage {
     /**
      * This method is used to get the image of the card from its class
-     * {@link model.deck.api.Card}.
+     * {@link Card}.
      * 
-     * @param card
-     *             Set of {@link model.deck.api.Card} to be showed.
-     * @return
-     *         List of {@link javax.swing.ImageIcon} of the card.
+     * @param card Set of {@link Card} to be showed.
+     * @return List of {@link ImageIcon} of the card.
      */
     List<ImageIcon> getCardImage(Set<Card> card);
 
     /**
-     * This method is used to get the image of the card back.
+     * This method is used to get the {@link ImageIcon} of the card back.
      * Is used to show hiden card.
      * 
-     * @param numerOfBack
-     *                    Number of back card to be showed.
-     * @return
-     *         List of {@link javax.swing.ImageIcon} of the back card.
+     * @param numerOfBack Number of back card to be showed.
+     * @return List of {@link ImageIcon} of the back card.
      */
     List<ImageIcon> getBackCardImage(int numerOfBack);
 
@@ -38,12 +34,10 @@ public interface CardGetterImage {
      * This method is used to get the image of the showed card and the hiden
      * card on the table .
      * 
-     * @param card
-     *             Set of card to be showed , its size must be under 5 card.
-     * @return
-     *         List of {@link javax.swing.ImageIcon} of the card and the back card.
+     * @param card Set of card to be showed , its size must be under 5 card.
+     * @return List of {@link ImageIcon} of the card and the back card.
      * @throws IllegalArgumentException
-     *                                  If the number of {@link model.deck.api.Card}
+     *                                  If the number of {@link Card}
      *                                  is over the limit.
      */
     List<ImageIcon> getTableCardImage(Set<Card> card);
